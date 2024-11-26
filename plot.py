bcbff791ad110e094b70464938c581c2df775bbf
HassaanAi <hassaanxhk11@gmail.com>
2024-11-26 13:37:38 +0500

    edited plot.py locally

--- plot.py
+++ plot.py
@@ -5,6 +5,7 @@ print("changes from Github remotely")
 data = pd.read_csv("Mall_customers.csv")
 
 # Plot a scatter plot for 'Annual Income (k$)' vs 'Spending Score (1-100)'
+plt.title("Updated Height vs Weight Chart")
 data.plot(x='Annual Income (k$)', y='Spending Score (1-100)', kind='scatter', title='Annual Income vs Spending Score')
 plt.xlabel("Annual Income (k$)")
 plt.ylabel("Spending Score (1-100)")
